435c742ff1fb0210cc8727a89a1110fbe10f85d6
David Shepherd <davidshepherd7@gmail.com>
2015-06-20 15:11:51 +0100

    Add script for local previewing

---
+++ preview.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+# Kill old jekyll
+pkill jekyll -9 || echo "No old jekyll"
+
+# Open site in 2 seconds (when jekyll is up)
+sleep 2s && firefox http://localhost:4000/ &
+
+# Rebuild and serve site, jekyll will watch for changes and constantly
+# rebuild.
+jekyll serve
