6b77940b06ea15497b8719e70caa885e9ad0c14e
David Shepherd <davidshepherd7@gmail.com>
2016-09-25 18:48:51 +0100

    Intial work on adding quotes

---
+++ _quotes/my-clippings-parser.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env nodejs
+
+'use strict'
+
+const fs = require('fs')
+const moment = require('moment')
+
+fs.readFile('/home/david/Dropbox/ebooks/Unknown/My Clippings (185)/My Clippings - Unknown.txt', 'utf8', function (err,fileString) {
+	if (err) {
+		return console.log(err)
+	}
+	const data = parseFile(fileString)
+	const md = formatMarkdown(data)
+	console.log(md)
+})
+
+
+function parseFile(fileString) {
+	return fileString
+		.split('==========')
+		.filter(x => x.includes("- Your Highlight")) // Filter out bookmarks
+		.map(parse)
+		.filter(x => x.quote && x.title && x.author)
+}
+
+
+function parse(str) {
+	const temp = str.split(/\n/)
+	const book = temp[1]
+	const highlight = temp[2]
+	const quote = temp.slice(3)
+		.map(s => s.trim())
+		.filter(s => s !== "")
+		.join(' ')
+
+	let author, title
+	const matched = book.match(/^(.*) \((.*)\)/)
+	if(matched) {
+		title = matched[1]
+		author = normaliseAuthor(matched[2])
+	}
+
+	const matchedHighlight = highlight.match(/Added on (.*)/)
+	let highlightDate
+	if(matchedHighlight) {
+		// highlightDate = moment(matchedHighlight[1], true).toDate()
+	}
+
+	return {author, title, quote, highlightDate}
+}
+
+
+
+function formatMarkdown(data) {
+	return data
+		.sort(d => d.author)
+		.map(d => `<blockquote class="ds-blockquote"> ${d.quote} </blockquote> \n\n <p class="ds-quote-attribution"> — ${d.author}, "${d.title}"</p>`)
+		.join("\n\n")
+}
+
+
+function normaliseAuthor(auth) {
+	if(auth.includes(",")) {
+		const a = auth.split(",")
+		return a[1].trim() + " " + a[0].trim()
+	}
+	else {
+		return auth
+	}
+}
--- build.sh
+++ build.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
+
 set -o errexit
 set -o nounset
+
+SCRIPTDIR="$( cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd )"
+cd "$SCRIPTDIR"
+
+
+# Generate quotes page
+cat <<EOF > quotes.md
+---
+layout: default
+title: "Quotes"
+---
+
+
+$(./_quotes/my-clippings-parser.js)
+EOF
 
 # Build html files
 jekyll build
