b235648c96fe7ee6b41e8bb0f12c68d660313db4
David Shepherd <davidshepherd7@gmail.com>
2022-03-13 09:21:43 +0000

    Remove quotes

--- build.sh
+++ build.sh
@@ -7,20 +7,6 @@ SCRIPTDIR="$( cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd )"
 cd "$SCRIPTDIR"
 
 
-# Generate quotes page
-quote_file=($HOME/Dropbox/ebooks/Kindle/My\ Clippings*/*.txt)
-cat <<EOF > quotes.md
----
-layout: default
-title: "Quotes"
----
-
-
-$(./_quotes/my-clippings-parser.js "${quote_file[@]}")
-
-$(./_quotes/my-clippings-parser.js "../manual_quotes.txt")
-EOF
-
 bundle install
 
 # Build html files
