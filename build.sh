604f88bfc4378cbd588109b1a24f5297a6fca529
David Shepherd <davidshepherd7@gmail.com>
2023-10-21 11:42:34 +0000

    Deploy to /var instead of home directory

--- build.sh
+++ build.sh
@@ -15,4 +15,7 @@ jekyll build --baseurl http://107.22.129.208/
 # https://aws.amazon.com/premiumsupport/knowledge-center/user-data-replace-key-pair-ec2/
 
 # Copy to servers
-rsync -aqzP --delete ./_site/ aws:~/public_html
+rsync --archive --compress --delete -i ./_site/ tech-trickery-aws:/var/www/html \
+      --rsync-path="sudo rsync" \
+      -g --groupmap '*:www-data' \
+      -u --usermap '*:www-data'
