37740be053f9abb7a4a07ccbaa48268c39a533c6
David Nikel-Shepherd <davidshepherd7@gmail.com>
2024-06-05 20:14:15 +0000

    Deployments to aws with podman

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,12 +5,27 @@ set -o pipefail
 # Portably go to the directory that this script is in
 cd "$(dirname ${BASH_SOURCE[0]})"
 
-# https://aws.amazon.com/premiumsupport/knowledge-center/user-data-replace-key-pair-ec2/
+./build.sh
 
-# # Copy to servers
-# rsync --archive --compress --delete -i ./_site/ tech-trickery-aws:/var/www/html \
-    #       --rsync-path="sudo rsync" \
-    #       -g --groupmap '*:www-data' \
-    #       -u --usermap '*:www-data'
+# Login to docker aws
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/w9r4b4d3
 
-# TODO
+# Push the container
+docker tag techtrickery-net:latest public.ecr.aws/w9r4b4d3/techtrickery-net:latest
+docker push public.ecr.aws/w9r4b4d3/techtrickery-net:latest
+
+# TODO probably have to ssh to the VM and pull? IDK
+
+
+# Commands to stand up a new VM:
+#
+# sudo apt update
+# sudo apt install podman
+# sudo podman pull public.ecr.aws/w9r4b4d3/techtrickery-net:latest
+# sudo podman run -d --name techtrickery-net -p 80:80 techtrickery-net
+#
+# sudo podman generate systemd --new --name --files techtrickery-net
+# sudo mv /home/admin/container-techtrickery-net.service /etc/systemd/system/
+# sudo systemctl enable container-techtrickery-net.service
+# sudo systemctl start container-techtrickery-net.service
+
